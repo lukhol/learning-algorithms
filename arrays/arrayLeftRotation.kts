9bf7c42b31f93e34739b109836bb90ca90710d83
lukhol <lukasz.holdrowicz@gmail.com>
2022-07-27 13:17:37 +0200

    Update arrayLeftRotation.kts

--- arrays/arrayLeftRotation.kts
+++ arrays/arrayLeftRotation.kts
@@ -2,6 +2,8 @@
 // Run me with kotlin arrayLeftRotation.kts
 // https://www.hackerrank.com/challenges/array-left-rotation/problem
 
+// TODO: try with S: O(1) - replace items in place 
+
 import java.util.*
 
 // S: O(n)
@@ -21,3 +23,16 @@ fun rotateLeft(d: Int, arr: Array<Int>): Array<Int> {
 val result = rotateLeft(4, arrayOf(1, 2, 3, 4, 5))
 println(Arrays.toString(result))
 
+// Can be also solved using brute force solution in S: O(1), T: O(nd)
+fun rotateLeft_bruteForce(d: Int, arr: Array<Int>): Array<Int> {
+    val count = if (d > arr.size) d % arr.size else d
+    for (i in 0 until count) {
+        for (idx in 1 until arr.size) {
+            val tmp = arr[idx]
+            arr[idx] = arr[idx - 1]
+            arr[idx - 1] = tmp
+        }
+    }
+
+    return arr
+}
