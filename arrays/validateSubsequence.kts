0c0e8390d475372ad287182bc4925f59e0825e39
lukhol <lukasz.holdrowicz@gmail.com>
2022-07-27 13:37:06 +0200

    Update validateSubsequence.kts

--- arrays/validateSubsequence.kts
+++ arrays/validateSubsequence.kts
@@ -2,28 +2,17 @@
 // Run me with kotlin validateSubsequence.kts
 // /questions/validate-subsequence
 
+// S: O(1)
+// T: O(n)
 fun isValidSubsequence(array: List<Int>, sequence: List<Int>): Boolean {
-    var arrayIdx = 0
-    var foundFromSequence = 0
-    for (sequenceElement in sequence) {
-        for (currArrayIdx in arrayIdx until array.size) {
-            val arrayElement = array[currArrayIdx]
-            if (arrayElement == sequenceElement) {
-                foundFromSequence += 1
-                arrayIdx = currArrayIdx + 1
-                break
-            }
-
-            if (currArrayIdx == array.size - 1) {
-                return false
-            }
-        }
-
+    var idxInArray = 0
+    for (item in array) {
+        if (idxInArray >= sequence.size) return true
+        if (sequence[idxInArray] == item) idxInArray++
     }
 
-    return foundFromSequence == sequence.size
+    return idxInArray == sequence.size
 }
-
 
 val result = isValidSubsequence(arrayListOf(5, 1, 22, 25, 6, -1, 8, 10), arrayListOf(1, 6, -1, 10))
 println(result)
