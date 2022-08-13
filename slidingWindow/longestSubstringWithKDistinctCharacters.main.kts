74d86f2847c5aad4e2fa1a64e401980a47a83ce5
lukhol <lukasz.holdrowicz@gmail.com>
2022-08-13 18:31:42 +0200

    Create longestSubstringWithKDistinctCharacters.main.kts

---
+++ slidingWindow/longestSubstringWithKDistinctCharacters.main.kts
@@ -0,0 +1,43 @@
+#!/usr/bin/env kotlin
+
+// Given a string, find the length of the longest substring
+// in it with no more than K distinct characters.
+// S: O(k)
+// T: O(n)
+fun longestSubstringWithKDistinctCharacters(str: String, k: Int): Int {
+    val letters = hashMapOf<Char, Int>()
+    var longest = 0
+    var startIdx = 0
+
+    for (endIdx in str.indices) {
+        letters.compute(str[endIdx]) { _, value ->
+            if (value == null) 1 else value + 1
+        }
+
+        while(letters.size > k) {
+            val letter = str[startIdx]
+            if(letters.containsKey(letter)) {
+                val newCount = letters[letter]!! - 1
+                if (newCount == 0) {
+                    letters.remove(letter)
+                } else {
+                    letters[letter] = newCount
+                }
+            }
+            startIdx++
+        }
+
+        longest = Math.max(longest, endIdx - startIdx + 1)
+    }
+
+    return longest
+}
+
+val result = longestSubstringWithKDistinctCharacters("araaci", 2)
+assert(result == 4)
+
+val result2 = longestSubstringWithKDistinctCharacters("araaci", 1)
+assert(result2 == 2)
+
+val result3 = longestSubstringWithKDistinctCharacters("cbbebi", 3)
+assert(result3 == 5)
