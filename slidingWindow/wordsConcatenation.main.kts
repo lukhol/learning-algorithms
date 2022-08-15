e0974219b084cf171b1c9f9d9080f1e9eaf5a107
lukhol <lukasz.holdrowicz@gmail.com>
2022-08-15 12:53:09 +0200

    Update wordsConcatenation.main.kts

--- slidingWindow/wordsConcatenation.main.kts
+++ slidingWindow/wordsConcatenation.main.kts
@@ -5,8 +5,8 @@
 // the given words exactly once without any overlapping of words.
 // It is given that all words are of the same length.
 
-// S:
-// T:
+// S: O(k) where k is number of words
+// T: O(nk) where n is number of letters in string and k is number of words
 fun wordsConcatenation(str: String, words: List<String>): List<Int> {
     if (words.isEmpty()) return emptyList()
     val singleWordLength = words[0].length
@@ -14,9 +14,11 @@ fun wordsConcatenation(str: String, words: List<String>): List<Int> {
 
     val wordCountMap = words.groupingBy { it }.eachCount().toMutableMap()
     val resultIndices = arrayListOf<Int>()
+    var count = 0
 
-    for (endIdx in singleWordLength - 1 .. str.lastIndex) {
-        val startIdx = endIdx - singleWordLength + 1
+    for (startIdx in 0 .. str.length - singleWordLength * words.size) {
+        val endIdx = startIdx + singleWordLength - 1
+
         fun backtrack(leftIdx: Int, rightIdx: Int, matchedCount: Int): Boolean {
             if (matchedCount == words.size) return true
             if (rightIdx > str.lastIndex) return false
