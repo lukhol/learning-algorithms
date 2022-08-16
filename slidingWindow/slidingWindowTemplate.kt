2c3a4f3b26ffdf2bb6a5edc648b86f89fdc06772
lukhol <lukasz.holdrowicz@gmail.com>
2022-08-16 10:32:44 +0200

    Create slidingWindowTemplate.kt

---
+++ slidingWindow/slidingWindowTemplate.kt
@@ -0,0 +1,44 @@
+// 1. Window can be of fixed length or variable length
+// 2. For most problems it's enough to iterate over an array with `endIndex` variable using for loop.
+//    `startIndex` variable can be increased/decreased manually. By moving `startIndex` sliding window can be
+//    a) shrinked - incrementing startIdx
+//    b) stay fixed - do not touch startIdx
+//    c) expanded - decrease startIdx
+//    Generally `startIdx` can be changed using while loop with proper condition
+// 3. Some problems require 
+//    a) using a Map for counting occurences of letters/values/words.
+//    b) using counter of occurrences
+//    c) using lenght variable
+
+fun slidingWindow(input: List<Int>, someInput: String) {
+    var startIdx = 0
+    var mapping: Map<Any, Int> = someInput
+        .split("")
+        .groupingBy { it }
+        .eachCount() // Any can be replaced by word/letter/number and map to number of occurrences
+
+    // or
+    var length = 0
+    // or
+    var longest = 0 // or max
+    // or
+    var count = 0
+
+    // Main for loop iterate with endIndex
+    for (endIdx in input.indices) {
+
+        // a) startIdx is typically moved using inner while loop
+        while(someCondition) {
+            startIdx++
+            // or
+            startIdx--
+        }
+
+        // b) startIdx can be also moved just once on some condition
+        if (someCondition) {
+            startIdx++
+            // or
+            startIdx--
+        }
+    }
+}
