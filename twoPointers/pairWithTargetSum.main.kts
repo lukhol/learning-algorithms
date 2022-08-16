0854d0a80b53a795ef136b4427eb11763a640570
lukhol <lukasz.holdrowicz@gmail.com>
2022-08-16 08:57:03 +0200

    Create pairWithTargetSum.main.kts

---
+++ twoPointers/pairWithTargetSum.main.kts
@@ -0,0 +1,30 @@
+#!/usr/bin/envkotlin kotlin
+
+// Given an array of sorted numbers and a target sum, find a pair in
+// the array whose sum is equal to the given target.
+// Write a function to return the indices of the two numbers (i.e. the pair)
+// such that they add up to the given target
+
+fun search(arr: List<Int>, targetSum: Int): List<Int> {
+    var leftPointer = 0
+    var rightPointer = arr.size - 1
+
+    while (leftPointer <= rightPointer) {
+        val left = arr[leftPointer]
+        val right = arr[rightPointer]
+        val currentSum = left + right
+        if (currentSum == targetSum) {
+            return listOf(leftPointer, rightPointer)
+        } else if (currentSum > targetSum) {
+            rightPointer--
+        } else {
+            leftPointer++
+        }
+    }
+
+    return arrayListOf(-1, -1)
+}
+
+println(search(arrayListOf(1, 2, 3, 4, 6), 6))
+println(search(arrayListOf(2, 5, 9, 11), 11))
+println(search(arrayListOf(2, 5, 9, 11), 120))
