be2556279b2581f0eba6f41d33ce5a8752f4309f
lukhol <lukasz.holdrowicz@gmail.com>
2022-08-26 22:01:52 +0200

    Update startOfLinkedListCycle.main.kts

--- fastSlowPointers/startOfLinkedListCycle.main.kts
+++ fastSlowPointers/startOfLinkedListCycle.main.kts
@@ -1,12 +1,83 @@
 #!/usr/bin/env kotlin
 
-// Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle. 
+// Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
 
 class LinkedList(val value: Int, var next: LinkedList? = null)
 
+// Solution is to find length of the cycle, then iterate from the beginning
+// by cycle length iteration. Then iterate from that position with one pointer
+// and from the beginning with other pointer. When they meet then we found
+// beginning of the cycle
+//
+// 1. Other approach is just to iterate with slow and fast pointer until they meet
+// 2. Then take again two pointers - first is the head, and second is that meet node from pointer 1
+// 3. Iterate using those two pointers until they meet. When meet then return.
+//
+// 1-1, 2-3, 3-5, 4-7, 5-3, 6-5, 7-7
+// 1 -> 2 -> 3 -> 4 -> 5
+//           |         |
+//           8 <- 7 <- 6
+// S: O(1)
+// T: O(n)
 fun findCycleStart(head: LinkedList): LinkedList {
-  
-  // TODO
+    var cycleLength = findCycleLength(head)
+    var slow: LinkedList? = head
+    while (cycleLength-- > 0) {
+        slow = slow?.next
+    }
+
+    var movingHead: LinkedList? = head
+    while (movingHead != slow) {
+        movingHead = movingHead?.next
+        slow = slow?.next
+    }
+
+    return movingHead!!
+}
+
+fun findCycleLength(head: LinkedList?): Int {
+    fun calculateLength(head: LinkedList?): Int {
+        if (head?.next == head) return 0
+        var slow = head?.next
+        var count = 1
+        while(slow != head) {
+            count++
+            slow = slow?.next
+        }
+
+        return count
+    }
+
+    var slow = head
+    var fast = head
+
+    while (fast?.next != null) {
+        slow = slow?.next
+        fast = fast.next?.next
+        if (slow == fast) {
+            return calculateLength(slow)
+        }
+    }
+
+    return 0
+}
+
+fun findLoop(head: LinkedList?): LinkedList? {
+    var slow = head
+    var fast = head
+    while (fast?.next != null) {
+        slow = slow?.next
+        fast = fast?.next?.next
+        if (slow == fast) break
+    }
+
+    slow = head
+    while (slow != fast) {
+        slow = slow?.next
+        fast = fast?.next
+    }
+
+    return slow
 }
 
 val node1 = LinkedList(1)
