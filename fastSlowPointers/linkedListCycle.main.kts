5a4f0a123ffb9a8e0e93a8889bd04f8b656e2902
lukhol <lukasz.holdrowicz@gmail.com>
2022-08-26 22:40:13 +0200

    Update linkedListCycle.main.kts

--- fastSlowPointers/linkedListCycle.main.kts
+++ fastSlowPointers/linkedListCycle.main.kts
@@ -1,5 +1,5 @@
 #!/usr/bin/env kotlin
-    
+
 // Given the head of a Singly LinkedList, write a function to
 // determine if the LinkedList has a cycle in it or not.
 
@@ -44,15 +44,15 @@ println(!hasCycle(node1))
 
 // Alternative
 fun hasCycle2(head: LinkedList?): Boolean {
-  if (head == null) return false
-  var slow = head
-  var fast =  head
-  
-while (fast != null && fast!!.next != null) {
-    slow = slow?.next
-    fast = fast?.next?.next
-    if (slow == fast) return true
-}
-  
-  return false
+    if (head == null) return false
+    var slow = head
+    var fast =  head
+
+    while (fast != null && fast!!.next != null) {
+        slow = slow?.next
+        fast = fast?.next?.next
+        if (slow == fast) return true
+    }
+
+    return false
 }
