45d9f7438999f62367b81dd8f0358f1772f9a364
lukhol <lukasz.holdrowicz@gmail.com>
2022-08-26 15:31:10 +0200

    Update linkedListCycleLength.main.kts

--- fastSlowPointers/linkedListCycleLength.main.kts
+++ fastSlowPointers/linkedListCycleLength.main.kts
@@ -1,12 +1,39 @@
 #!/usr/bin/env kotlin
-
 // Given the head of a LinkedList with a cycle, find the length of the cycle.
 
 class LinkedList(val value: Int, var next: LinkedList? = null)
 
+// 1-1, 2-3, 3-5, 4-7, 5-5 (meet) -> iterate from 5 to 5 again counting
+// 1 -> 2 -> 3 -> 4
+//           |    |
+//           6 <- 5
+// S: O(1)
+// T: O(n)
 fun findCycleLength(head: LinkedList?): Int {
-  // TODO
-  return 0
+    fun calculateLength(head: LinkedList?): Int {
+        if (head?.next == head) return 0
+        var slow = head?.next
+        var count = 1
+        while(slow != head) {
+            count++
+            slow = slow?.next
+        }
+
+        return count
+    }
+
+    var slow = head
+    var fast = head
+
+    while (fast?.next != null) {
+        slow = slow?.next
+        fast = fast.next?.next
+        if (slow == fast) {
+            return calculateLength(slow)
+        }
+    }
+
+    return 0
 }
 
 val node1 = LinkedList(1)
@@ -21,6 +48,6 @@ node2.next = node3
 node3.next = node4
 node4.next = node5
 node5.next = node6
-node6.next = node1
+node6.next = node4
 
-println(findCycleLength(node1) == TODO)
+println(findCycleLength(node1) == 3)
