465cb81f6abc175ff77623452ab980482b8e5a68
lukhol <lukasz.holdrowicz@gmail.com>
2022-08-28 12:40:21 +0200

    Create reverseSubList.,main.kts

---
+++ linkedList/reverseSubList.,main.kts
@@ -0,0 +1,11 @@
+#!/usr/bin/env kotlin
+
+// Given the head of LinkedList and two positions `p` and `q`, reverse the LinkedList from position `p` to `q`
+
+class LinkedList(val value: Int, var next: LinkedList? = null)
+
+fun reverseLinkedListInPositions(head: LinkedList?, p: Int, q: Int): LinkedList? {
+  // TODO
+}
+
+// TODO
