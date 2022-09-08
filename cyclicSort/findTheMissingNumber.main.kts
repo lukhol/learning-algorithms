b73f39e4f6f8cd83be4d57d6112a5df9e52142d3
lukhol <lukasz.holdrowicz@gmail.com>
2022-09-08 12:03:04 +0200

    Update findTheMissingNumber.main.kts

--- cyclicSort/findTheMissingNumber.main.kts
+++ cyclicSort/findTheMissingNumber.main.kts
@@ -1,4 +1,4 @@
-// #!/usr/bin/envkotlin kotlin
+#!/usr/bin/envkotlin kotlin
 
 // We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’.
 // Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
