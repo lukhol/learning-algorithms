b12133556983f0852d56eadcbe14036d988a3fcc
lukhol <lukasz.holdrowicz@gmail.com>
2022-09-14 15:33:02 +0200

    Update findTheSmallestMissingPositiveNumber.main.kts

--- cyclicSort/findTheSmallestMissingPositiveNumber.main.kts
+++ cyclicSort/findTheSmallestMissingPositiveNumber.main.kts
@@ -18,6 +18,13 @@ fun main() {
     println(findTheSmallestMissingPositiveNumber(arrayListOf(-3, 1, 5, 4, 2)) == 3)
     println(findTheSmallestMissingPositiveNumber(arrayListOf(3, -2, 0, 1, 2)) == 4)
     println(findTheSmallestMissingPositiveNumber(arrayListOf(3, 2, 5, 1)) == 4)
+    println(findTheSmallestMissingPositiveNumber(mutableListOf(3, 4, -1, 1)) == 2)
+    println(findTheSmallestMissingPositiveNumber(mutableListOf(1, 2, 0)) == 3)
+    println(findTheSmallestMissingPositiveNumber(mutableListOf(1, 2, 3)) == 4)
+    println(findTheSmallestMissingPositiveNumber(mutableListOf(1, 2, 4)) == 3)
+    println(findTheSmallestMissingPositiveNumber(mutableListOf(0, 2, 4)) == 1)
+    println(findTheSmallestMissingPositiveNumber(mutableListOf(1, 2, 4, 5, 6, 7, 8, 9)) == 3)
+    println(findTheSmallestMissingPositiveNumber(mutableListOf(1, 2, 4, 5, 6, 7, 8, 9, 10)) == 3)
 }
 
 main()
