eda7be828f72370030e05fcee173edcc528e498d
lukhol <lukasz.holdrowicz@gmail.com>
2022-09-21 12:41:37 +0200

    Update rightViewOfABinaryTree.main.kts

--- breadthFirstSearch/rightViewOfABinaryTree.main.kts
+++ breadthFirstSearch/rightViewOfABinaryTree.main.kts
@@ -14,18 +14,38 @@ class TreeNode (val value: Int) {
 }
 
 fun main() {
-    fun traverse(root: TreeNode): TreeNode {
-        TODO()
+    // S: O(n)
+    // T: O(n)
+    fun traverse(root: TreeNode): List<Int> {
+        val result = arrayListOf<Int>()
+        val queue: Queue<TreeNode> = LinkedList()
+        queue.add(root)
+        
+        while(queue.isNotEmpty()) {
+            var last = queue.peek()
+            for (i in 0 until queue.size) {
+               	last = queue.poll()
+                last.left?.let { queue.add(it) }
+                last.right?.let { queue.add(it) }
+            }
+            result.add(last.value)
+        }
+        
+        return result
     }
     
-    // 	 12
-    // 	7  1
-    // 9  10 5
+    // 	  12
+    // 	 7  1
+    //  9  10 5
+    // 17
     val root = TreeNode(12)
     root.left = TreeNode(7)
     root.right = TreeNode(1)
     root.left?.left = TreeNode(9)
     root.right?.left = TreeNode(10)
     root.right?.right = TreeNode(5)
-   	traverse(root)
+    root.left?.left?.left = TreeNode(17)
+   	println(traverse(root) == listOf(12, 1, 5, 17))
+    root.right?.left?.right = TreeNode(18)
+   	println(traverse(root) == listOf(12, 1, 5, 18))
 }
