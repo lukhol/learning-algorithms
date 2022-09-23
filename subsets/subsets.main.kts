e788928660efefed5d26c60602f84bf9bd399e76
lukhol <lukasz.holdrowicz@gmail.com>
2022-09-23 21:23:05 +0200

    Update subsets.main.kts

--- subsets/subsets.main.kts
+++ subsets/subsets.main.kts
@@ -1,8 +1,31 @@
-### Subset
+#!/usr/bin/envkotlin kotlin
 
-#### Description
+fun subsets(arr: List<Int>): List<List<Int>> {
+    val result = arrayListOf(listOf<Int>())
 
-#### Template
-```kotlin
+    for (i in 0 .. arr.lastIndex) {
+        val resultSize = result.size
+        for (idx in 0 until resultSize) {
+            result.add(result[idx].plus(arr[i]))
+        }
+    }
 
-```
+    return result
+}
+
+println(subsets(listOf(1, 3)) == listOf(
+    listOf(),
+    listOf(1),
+    listOf(3),
+    listOf(1, 3)
+))
+println(subsets(listOf(1, 5, 3)) == listOf(
+    listOf(),
+    listOf(1),
+    listOf(5),
+    listOf(3),
+    listOf(1, 5),
+    listOf(1, 3),
+    listOf(5, 3),
+    listOf(1, 5, 3)
+))
