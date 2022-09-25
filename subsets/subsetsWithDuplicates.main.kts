b83fe474e0307c525cccb71f6138d86fc4497e90
lukhol <lukasz.holdrowicz@gmail.com>
2022-09-25 10:13:13 +0200

    Update subsetsWithDuplicates.main.kts

--- subsets/subsetsWithDuplicates.main.kts
+++ subsets/subsetsWithDuplicates.main.kts
@@ -1,20 +1,25 @@
 #!/usr/bin/envkotlin kotlin
 
-fun subsetsWithDuplicates(arr: List<Int>): List<List<Int>> {
-    val sorted = arr.sorted()
+// Given a set of numbers that might contain duplicates, find all of its distinct subsets.
+// T: O(2^n)
+// S: O(2^n)
+fun subsetsWithDuplicates(nums: IntArray): List<List<Int>> {
+    nums.sort()
     val result = arrayListOf(listOf<Int>())
 
-    for (i in 0 .. arr.lastIndex) {
-        val from = if (i > 0 && sorted[i - 1] == sorted[i]) result.size / 2 else 0
-        for (idx in from until result.size) {
-            result.add(result[idx].plus(sorted[i]))
+    var endIdx = 0
+    for (i in 0 .. nums.lastIndex) {
+        val startIdx = if (i > 0 && nums[i - 1] == nums[i]) endIdx + 1 else 0
+        endIdx = result.lastIndex
+        for (idx in startIdx .. endIdx) {
+            result.add(result[idx].plus(nums[i]))
         }
     }
 
     return result
 }
 
-println(subsetsWithDuplicates(listOf(1, 3, 3)) == listOf(
+println(subsetsWithDuplicates(intArrayOf(1, 3, 3)) == listOf(
     listOf(),
     listOf(1),
     listOf(3),
@@ -22,7 +27,7 @@ println(subsetsWithDuplicates(listOf(1, 3, 3)) == listOf(
     listOf(3, 3),
     listOf(1, 3, 3),
 ))
-println(subsetsWithDuplicates(listOf(1, 5, 3, 3)) == listOf(
+println(subsetsWithDuplicates(intArrayOf(1, 5, 3, 3)) == listOf(
     listOf(),
     listOf(1),
     listOf(3),
