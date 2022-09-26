26d7974768911db8ccc1c50b5322cbcb80464020
lukhol <lukasz.holdrowicz@gmail.com>
2022-09-26 14:14:09 +0200

    Update balancedParentheses.main.kts

--- subsets/balancedParentheses.main.kts
+++ subsets/balancedParentheses.main.kts
@@ -1,12 +1,37 @@
 #!/usr/bin/envkotlin kotlin
 // https://leetcode.com/problems/generate-parentheses/solution/
-
 // For a given number ‘N’, write a function to generate all combination of ‘N’ pairs of balanced parentheses.
 
+import java.util.*
+
 class Solution {
-    class BracketsCount(var opening: Int, var closing: Int, var value: String)
+    data class BracketsCount(val opening: Int, val closing: Int, val value: String)
     
-    fun generateParenthesis(n: Int): List<String> {
+    fun generateParenthesisBfs(n: Int): List<String> {
+        val result = arrayListOf<String>()
+        val queue = LinkedList<BracketsCount>()
+        queue.add(BracketsCount(0, 0, ""))
+        
+        while (queue.isNotEmpty()) {
+            val current = queue.poll()
+            if (current.value.length == n * 2) {
+                result.add(current.value)    
+                continue
+            }
+            
+            if (current.opening < n) {
+                queue.add(BracketsCount(current.opening + 1, current.closing, current.value + "("))
+            }
+            
+            if (current.opening > current.closing && current.closing < n) {
+                queue.add(BracketsCount(current.opening, current.closing + 1, current.value + ")"))
+            }
+        }
+        
+        return result
+    }
+    
+    fun generateParenthesisDfs(n: Int): List<String> {
         var result = arrayListOf<String>()
         
         fun dfs(used: Int, opening: Int, closing: Int, sb: StringBuilder) {
@@ -25,11 +50,12 @@ class Solution {
                 dfs(used + 1, opening, closing + 1, sb)
                 sb.deleteCharAt(sb.length - 1)
             }
-    }
+        }
         
         dfs(n, 0, 0, StringBuilder())
         return result
     }
 }
 
-println(Solution().generateParenthesis(3) == listOf("((()))", "(()())", "(())()", "()(())", "()()()"))
+println(Solution().generateParenthesisDfs(3) == listOf("((()))", "(()())", "(())()", "()(())", "()()()"))
+println(Solution().generateParenthesisBfs(3) == listOf("((()))", "(()())", "(())()", "()(())", "()()()"))
