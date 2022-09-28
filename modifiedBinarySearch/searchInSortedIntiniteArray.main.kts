7dd15332114b807a4c618c4878c17c61cbc43a11
lukhol <lukasz.holdrowicz@gmail.com>
2022-09-28 10:29:43 +0200

    Create searchInSortedIntiniteArray.main.kts

---
+++ modifiedBinarySearch/searchInSortedIntiniteArray.main.kts
@@ -0,0 +1,37 @@
+#!/usr/bin/envkotlin kotlin
+// https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size
+
+class ArrayReader(private val array: IntArray) {
+  fun get(index: Int): Int {
+    return if (index >= array.size) return Int.MAX_VALUE else return array[index]
+  }
+}
+
+class Solution {
+    // S: O(1)
+    // T: O(logn)
+    fun search(reader: ArrayReader, target: Int): Int {
+        var startIdx = 0
+        var endIdx = Int.MAX_VALUE
+        while (endIdx >= startIdx) {
+            val middleIdx = startIdx + (endIdx - startIdx) / 2
+            if (reader.get(middleIdx) == Int.MAX_VALUE) {
+                endIdx = middleIdx - 1    
+            } else if (target > reader.get(middleIdx)) {
+                startIdx = middleIdx + 1
+            } else if (target < reader.get(middleIdx)) {
+                endIdx = middleIdx - 1
+            } else {
+                return middleIdx
+            }
+        }
+        
+        return -1
+    }
+}
+
+fun main() {
+    println(Solution().search(ArrayReader(intArrayOf(-1, 0, 3, 5, 9, 12)), 9) == 4)
+    println(Solution().search(ArrayReader(intArrayOf(-1,0,3,5,9,12)), 2) == -1)
+
+}
