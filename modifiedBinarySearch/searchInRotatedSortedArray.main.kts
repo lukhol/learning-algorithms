82c165ba742748daa3d34e057cef8d76a63673a5
lukhol <lukasz.holdrowicz@gmail.com>
2022-10-03 14:27:20 +0100

    Update searchInRotatedSortedArray.main.kts

--- modifiedBinarySearch/searchInRotatedSortedArray.main.kts
+++ modifiedBinarySearch/searchInRotatedSortedArray.main.kts
@@ -10,6 +10,35 @@
 // T: O(3logn) = O(logn)
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.lastIndex
+
+        while (end >= start) {
+            val mid = start + (end - start) / 2
+
+            if (nums[mid] == target) {
+                return mid
+            } else if (nums[start] <= nums[mid]) {
+                // left side is properly sorted
+                if (target >= nums[start] && target <= nums[mid]) {
+                    end = mid - 1
+                } else {
+                    start = mid + 1
+                }
+            } else if (nums[end] > nums[mid]) {
+                // right side is properly sorted
+                if (target >= nums[mid] && target <= nums[end]) {
+                    start = mid + 1
+                } else {
+                    end = mid - 1
+                }
+            }
+        }
+
+        return -1
+    }
+    
+    fun search_workingButNotNeededChecks(nums: IntArray, target: Int): Int {
         if (nums.isEmpty()) return -1
         if (nums.size == 1) {
             return if (nums.first() == target) 0 else -1
